01428d3fc94612b246a8859a4795b08c725bebad
Jogboms <jeremiahogbomo@gmail.com>
2021-05-11 19:35:28 +0200

    Clean up boilerplate

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        body: Center(child: Text('Hello world')),
+      ),
+    );
+  }
+}
